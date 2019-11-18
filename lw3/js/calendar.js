fcf12860eb1ae967748df25654cb53312b17acb7
Shinabuzza <31733836+Shinabuzza@users.noreply.github.com>
2019-11-18 23:02:08 +0300

    task 6 completed

--- lw3/js/calendar.js
+++ lw3/js/calendar.js
@@ -1,5 +1,17 @@
- var toggleNight = document.getElementById('night-mode');
+window.onload = function() {
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  var dateCells = document.querySelectorAll('.day');
+  for (let cell of dateCells) {
+    if (cell.innerText == currentDay) {
+      if (!cell.parentElement.classList.contains('off')) {
+        cell.classList.toggle('today');
+      }
+    }
+  }
+  var toggleNight = document.getElementById('night-mode');
   toggleNight.onclick = function() {
     var calendarWindow = document.getElementById('calendar-app');
     calendarWindow.classList.toggle('dark');
   }
+}
